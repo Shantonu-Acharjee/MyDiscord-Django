aa6803767ea8de36c13cb8d6d2a9745ba1c3e0d0
Shantonu-Acharjee <shantonuacharjee@gmail.com>
2023-03-11 13:00:52 +0600

    send variable to template

--- base/views.py
+++ base/views.py
@@ -2,8 +2,14 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 
+rooms = [
+    {'id' : 1, 'name': 'Lets learn python'},
+    {'id' : 2, 'name': 'Lets learn C++'},
+    {'id' : 3, 'name': 'Lets learn js'},
+]
+
 def home(request):
-    return render(request, 'home.html')
+    return render(request, 'home.html', {'rooms' : rooms})
 
 def room(request):
     return render(request, 'room.html')
